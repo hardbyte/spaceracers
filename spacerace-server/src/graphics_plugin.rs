3f0931ab3c84da3f4922e26b92951f3f449408db
Brian Thorne <brian@thorne.link>
2024-12-25 18:02:21 +1300

    🛫 Lobby UI

--- spacerace-server/src/graphics_plugin.rs
+++ spacerace-server/src/graphics_plugin.rs
@@ -1,7 +1,8 @@
 use bevy::app::{App, Plugin, PluginGroup, Startup};
 use bevy::color::Color;
-use bevy::prelude::{default, Camera2dBundle, ClearColor, Commands, Window, WindowPlugin};
-use bevy::DefaultPlugins;
+use bevy::prelude::{default, Camera2d, ClearColor, Commands};
+
+use crate::lobby_graphics_plugin::LobbyGraphicsPlugin;
 use bevy_rapier2d::prelude::RapierDebugRenderPlugin;
 
 // I'm using this to visualize the physics engine
@@ -9,10 +10,7 @@ use bevy_rapier2d::prelude::RapierDebugRenderPlugin;
 pub struct GraphicsPlugin;
 
 fn setup_graphics(mut commands: Commands) {
-    commands.spawn(Camera2dBundle {
-        transform: Default::default(),
-        ..default()
-    });
+    commands.spawn(Camera2d { ..default() });
 }
 
 impl Plugin for GraphicsPlugin {
@@ -25,6 +23,7 @@ impl Plugin for GraphicsPlugin {
         )));
 
         app.add_systems(Startup, setup_graphics);
-        app.add_plugins((RapierDebugRenderPlugin::default()));
+        app.add_plugins(RapierDebugRenderPlugin::default());
+        app.add_plugins(LobbyGraphicsPlugin);
     }
 }
